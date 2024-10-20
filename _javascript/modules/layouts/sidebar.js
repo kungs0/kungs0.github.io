bd104974cd7163091fa94e4dbcd2536260c4283d
kungs0 <viktorstietz@outlook.lv>
2024-10-20 09:31:58 +0200

    tryna add sidebar toogle

--- _javascript/modules/layouts/sidebar.js
+++ _javascript/modules/layouts/sidebar.js
@@ -4,4 +4,9 @@ import { sidebarExpand } from '../components/sidebar';
 export function initSidebar() {
   modeWatcher();
   sidebarExpand();
+
+  // Add event listener for desktop sidebar toggle
+  document.getElementById("sidebar-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("sidebar-collapsed"); // Добавляем/убираем класс, скрывающий сайдбар
+  });
 }
