af9d7157fb76db4302035af5804c9b9d8b36719e
kungs0 <viktorstietz@outlook.lv>
2024-10-20 09:54:45 +0200

    fix this out

--- _javascript/modules/layouts/sidebar.js
+++ _javascript/modules/layouts/sidebar.js
@@ -4,9 +4,4 @@ import { sidebarExpand } from '../components/sidebar';
 export function initSidebar() {
   modeWatcher();
   sidebarExpand();
-
-  // Добавляем обработчик события для переключения сайдбара на десктопе
-  document.getElementById("sidebar-toggle").addEventListener("click", function() {
-    document.body.classList.toggle("sidebar-collapsed"); // Добавляем/убираем класс, скрывающий сайдбар
-  });
 }
